d2632b7ddf320bbf5a7d8608a0c80eb9afb538e9
Philipp Schuster <pschuster@uni-koblenz.de>
2013-03-17 11:05:28 +0100

    Failed attempt to get packages

--- GetPackages.hs
+++ GetPackages.hs
@@ -4,14 +4,37 @@ import Distribution.Hackage.DB hiding (map)
 import Distribution.PackageDescription
 import qualified Data.Map as M
 import Distribution.Text
+import Network.HTTP
 
 import Control.Proxy
 
-packageNames :: (Proxy p,Monad m) => Hackage -> () -> Producer p String m ()
-packageNames hackage = fromListS $ [name ++ "-" ++ show (disp version) | name <- M.keys hackage, version <- M.keys (hackage M.! name)]
+import Data.ByteString.Lazy (ByteString)
+
+import Data.Time (getCurrentTime,UTCTime)
+
+packageNames :: (Proxy p,Monad m) => Hackage -> () -> Producer p (String,String) m ()
+packageNames hackage = fromListS $ [(name,show (disp version))| name <- M.keys hackage, version <- M.keys (hackage M.! name)]
 
 main :: IO ()
 main = do
+    time <- getCurrentTime
+    let foldername = "./packages-" ++ timeToName time
     hackage <- readHackage
-    print $ runWriter $ runProxy $ packageNames hackage >-> lengthD
+    runProxy $
+            packageNames hackage
+        >-> mapMD (\(name,version) -> simpleHTTP (getRequest (nameAndVersionToUrl (name,version))) >>= getResponseBody)
+        >-> blah
+
+nameAndVersionToUrl :: (String,String) -> String
+nameAndVersionToUrl = undefined
+
+blah :: (Proxy p) => () -> Consumer p ByteString IO ()
+blah = undefined
+
+
+timeToName :: UTCTime -> String
+timeToName = ("attempt"++) . replace '.' '-' . replace ':' '-' . replace ' ' '_' . show
+
+replace :: (Eq a) => a -> a -> [a] -> [a]
+replace a b = map (\ x -> if x==a then b else x)
 
