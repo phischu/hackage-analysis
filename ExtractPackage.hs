25c87043053f247413bf923f5bc5c856586d73e0
Philipp Schuster <pschuster@uni-koblenz.de>
2013-03-17 16:46:05 +0100

    Some more work on extraction

--- ExtractPackage.hs
+++ ExtractPackage.hs
@@ -9,9 +9,15 @@ import Paths
 extractPackage :: FilePath -> Action ()
 extractPackage out = do
     need ["ExtractPackage.hs"]
+    need [filenameToArchiveName out]
+    system' "tar" ["xzf",filenameToArchiveName out,"-C",targetDirectory out]
     writeFile' out []
 
+filenameToArchiveName :: FilePath -> FilePath
+filenameToArchiveName = undefined
 
+targetDirectory :: FilePath -> FilePath
+targetDirectory = undefined
 
 
 
