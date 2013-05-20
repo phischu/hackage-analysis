b7ff8015de3f429df976c8c2882e910ebdaaf0c0
Philipp Schuster <pschuster@uni-koblenz.de>
2013-05-20 16:09:01 +0200

    slightly increase number of packages for a test run

--- MasterPipe/EnumPackages.hs
+++ MasterPipe/EnumPackages.hs
@@ -7,21 +7,34 @@ import MasterPipe.Database
 import Database.PropertyGraph (PropertyGraphT,VertexId,newVertex)
 
 import Control.Proxy (Proxy,Producer,lift,liftP,respond)
-import Control.Proxy.Safe (ExceptionP,SafeIO)
+import Control.Proxy.Safe (ExceptionP,SafeIO,tryIO,catch)
 import Control.Proxy.Trans.State (StateP,put)
+import Control.Monad.Morph (hoist)
 
+import Control.Monad (forM_)
+import Control.Exception (SomeException)
+
+import Data.Text (pack)
 import Data.Map (empty)
+import Data.List (nub)
 
 
 enumpackagesS :: (Proxy p) => () -> Producer (ExceptionP (StateP VertexId p)) PackageName (PropertyGraphT SafeIO) ()
-enumpackagesS () = do
+enumpackagesS () = (do
 
 	rootvertex <- lift (newVertex empty)
 
-	packagevertex <- lift (insertVertex "PACKAGE" "packagename" "base" rootvertex)
+	packagelist <- hoist lift (tryIO (readFile "packages.list" >>= return . map read . lines))
 
-	liftP (put packagevertex)
+	let packagenames = nub (map (\(PackageVersion packagename _ _) -> packagename) packagelist)
 
-	respond "base"
+	forM_ packagenames (\packagename -> do
+	    packagevertex <- lift (insertVertex "PACKAGE" "packagename" (pack packagename) rootvertex)
+	    liftP (put packagevertex)
+	    respond packagename)
+
+        `catch`
+
+    (\e -> hoist lift (tryIO (print (e :: SomeException)))))
 
 
