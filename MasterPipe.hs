bd547602d854ac0b873b577dd990e4cb788da8b9
Philipp Schuster <pschuster@uni-koblenz.de>
2013-06-03 19:34:01 +0200

    try out single insertion into neo4j - way too slow

--- MasterPipe.hs
+++ MasterPipe.hs
@@ -2,14 +2,20 @@
 module MasterPipe where
 
 import Control.Proxy (Proxy,(>->),runProxy)
-import Control.Proxy.Safe (trySafeIO)
+import Control.Proxy.Safe (SafeIO,trySafeIO,tryIO)
 import Control.Proxy.Trans.Either (runEitherK)
 import Control.Proxy.Trans.State (evalStateK)
+import Control.Error (EitherT(EitherT),runEitherT)
+import Control.Monad.Morph (MFunctor,hoist)
+import Control.Monad.Trans.Free (FreeT(FreeT),runFreeT)
+import Control.Monad.IO.Class (MonadIO,liftIO)
+import Control.Exception (throw)
 
 import Data.Map.Strict (empty)
 
 import Database.PropertyGraph (newVertex)
-import Database.PropertyGraph.Neo4jBatch (runPropertyGraphT,add)
+import Database.PropertyGraph.Neo4jSingle (runPropertyGraphT)
+import Database.Neo4j (defaultClient)
 
 import MasterPipe.Types (PackageTree(PackageTree),PackageName,Version,ModuleName,Fragment(FunctionFragment))
 import MasterPipe.EnumPackages (enumpackagesS)
@@ -22,7 +28,7 @@ import MasterPipe.Fragment (fragmentD)
 
 masterpipe :: IO ()
 masterpipe = do
-    result <- trySafeIO $ runPropertyGraphT $ runProxy $ evalStateK undefined $ runEitherK $
+    result <- runEitherT $ runPropertyGraphT defaultClient $ hoist trySafeIO $ runProxy $ evalStateK undefined $ runEitherK $
         enumpackagesS >->
         enumVersionsD >->
         configureD >->
@@ -30,7 +36,12 @@ masterpipe = do
         preprocessD >->
         parseD >->
         fragmentD
-    case result of
-        (Left e,_) -> print e
-        (Right (),propertygraph) ->
-            add "localhost" 7474 propertygraph >>= either print (const (return ()))
+    print result
+
+instance (Functor f) => MFunctor (FreeT f) where
+    hoist nat freet = FreeT (nat (do
+        next <- runFreeT freet
+        return (fmap (hoist nat) next)))
+
+instance MonadIO SafeIO where
+    liftIO m = runProxy (runEitherK (const (tryIO m))) >>= either throw return
