546d9d76206dab2c4f82e5f1fbd7757937223e70
phischu <pschuster@uni-koblenz.de>
2014-01-22 12:27:45 +0100

    configure package

--- src/Instances.hs
+++ src/Instances.hs
@@ -2,6 +2,13 @@ module Instances where
 
 import Types (Repository,PackageName,VersionNumber,Instance)
 
+import Distribution.PackageDescription.Parse (readPackageDescription)
+import Distribution.PackageDescription.Configuration (finalizePackageDescription)
+import Distribution.Verbosity (silent)
+import Distribution.System (Platform(Platform),Arch(I386),OS(Linux))
+import Distribution.Compiler (CompilerId(CompilerId),CompilerFlavor(GHC))
+import Data.Version (Version(Version))
+
 import Data.Map (Map,traverseWithKey)
 
 
@@ -11,7 +18,15 @@ parseAllPackages = traverseWithKey (\packagename ->
 
 parsePackage :: PackageName -> FilePath -> IO Instance
 parsePackage packagename filepath = do
+    let cabalfilepath = filepath ++ packagename ++ ".cabal"
+    genericpackagedescription <- readPackageDescription silent cabalfilepath
+    let packagedescription = finalizePackageDescription [] (const True) defaultPlatform defaultCompiler [] genericpackagedescription
     putStrLn ("Parsing " ++ packagename ++ " at " ++ filepath)
     return undefined
 
+defaultPlatform :: Platform
+defaultPlatform = Platform I386 Linux
+
+defaultCompiler :: CompilerId
+defaultCompiler = CompilerId GHC (Version [7,6,3] [])
 
