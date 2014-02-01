2e3836f77f8573096b02cc7918924a3e41fd0a64
phischu <pschuster@uni-koblenz.de>
2014-02-01 13:30:29 +0100

    parts of fragmentation

--- src/Fragmentation.hs
+++ src/Fragmentation.hs
@@ -1,12 +1,41 @@
 module Fragmentation where
 
-import Common (ParsedRepository,traverseRepository)
+import Common (
+    ParsedRepository,traverseRepository,PackagePath,
+    loadPackage,PackageInformation(PackageError,PackageInformation))
+
+import qualified Language.Haskell.Exts.Annotated as HSE (Decl,SrcSpanInfo)
+
+import Language.Haskell.Names (Symbols)
+
+import Distribution.ModuleName (ModuleName)
+
+import Control.Monad (forM_)
 
 splitAndSaveAllDeclarations :: ParsedRepository -> IO ()
 splitAndSaveAllDeclarations parsedrepository = do
     flip traverseRepository parsedrepository (\_ _ packagepath -> do
-        return ())
+        splitAndSaveDeclarations parsedrepository packagepath)
     return ()
 
+splitAndSaveDeclarations :: ParsedRepository -> PackagePath -> IO ()
+splitAndSaveDeclarations parsedrepository packagepath = do
+    maybepackageinformation <- loadPackage packagepath
+    case maybepackageinformation of
+        Nothing -> return ()
+        Just (PackageError _) -> return ()
+        Just (PackageInformation modulenames dependencies) -> do
+            forM_ modulenames (\modulename -> do
+                declarations <- splitModule parsedrepository packagepath modulename
+                saveDeclarations packagepath modulename declarations)
 
+splitModule :: ParsedRepository -> PackagePath -> ModuleName -> IO [Declaration]
+splitModule = undefined
+
+saveDeclarations :: PackagePath -> ModuleName -> [Declaration] -> IO ()
+saveDeclarations = undefined
+
+data Declaration = Declaration Genre DeclarationAST Symbols Symbols
+data Genre = Genre
+type DeclarationAST = HSE.Decl HSE.SrcSpanInfo
 
