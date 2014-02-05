3759880c0c252ec9d7655acb5c49998848f8afa7
phischu <pschuster@uni-koblenz.de>
2014-02-05 16:46:18 +0100

    stubs for loading

--- src/Insertion.hs
+++ src/Insertion.hs
@@ -1,29 +1,47 @@
 module Insertion where
 
 import Common (
-    ParsedRepository,traverseRepository,
-    PackageName,VersionNumber,PackageInformation,
-    ModuleError,Declaration,NameErrors)
+    ParsedRepository,traverseRepository,PackagePath,
+    PackageName,VersionNumber,
+    PackageInformation(PackageError,PackageInformation),PackageError,
+    ModuleError,Declaration,NameErrors,
+    loadPackage)
 
 import Distribution.ModuleName (ModuleName)
+import Distribution.Package (Dependency)
 
 import Data.Map (Map)
+import qualified Data.Map as Map (fromList)
 
 import Control.Monad (void)
 
 insertAllPackages :: ParsedRepository -> IO ()
 insertAllPackages =
     void . (traverseRepository (\packagename versionnumber packagepath -> do
-        packageinformation <- undefined
-        modulemap <- undefined
-        maybenameerrors <- undefined
-        insertPackage packagename versionnumber packageinformation modulemap maybenameerrors))
+        maybepackageinformation <- loadPackage packagepath
+        case maybepackageinformation of
+            Nothing -> return ()
+            Just (PackageError packageerror) -> insertPackageError packagename versionnumber packageerror
+            Just (PackageInformation modulenames dependencies) -> do
+                modulemap <- loadModuleDeclarations packagepath modulenames >>= return . Map.fromList
+                maybenameerrors <- loadNameErrors packagepath
+                insertPackage packagename versionnumber dependencies modulemap maybenameerrors))
+
+loadModuleDeclarations :: PackagePath -> [ModuleName] -> IO [(ModuleName,Either ModuleError [Declaration])]
+loadModuleDeclarations = undefined
+
+loadNameErrors :: PackagePath -> IO (Maybe NameErrors)
+loadNameErrors = undefined
 
 insertPackage ::
     PackageName ->
     VersionNumber ->
-    PackageInformation ->
+    [Dependency] ->
     Map ModuleName (Either ModuleError [Declaration]) ->
     Maybe NameErrors ->
     IO ()
 insertPackage = undefined
+
+insertPackageError :: PackageName -> VersionNumber -> PackageError -> IO ()
+insertPackageError = undefined
+
