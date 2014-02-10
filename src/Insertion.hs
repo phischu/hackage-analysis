26b25b1d1128144f38f95304b7b25ecdf9cf7ce1
phischu <pschuster@uni-koblenz.de>
2014-02-10 12:40:08 +0100

    insert symbols

--- src/Insertion.hs
+++ src/Insertion.hs
@@ -10,14 +10,20 @@ import Common (
 
 import Web.Neo (NeoT,defaultRunNeoT,cypher)
 
+import Language.Haskell.Names (
+    Symbols(Symbols),SymValueInfo(..),SymTypeInfo(..),OrigName(OrigName),GName(GName))
+
 import Distribution.ModuleName (ModuleName)
 import Distribution.Package (Dependency(Dependency))
 import Distribution.Text (display)
 
-import Data.Aeson (Value,object,(.=))
+import Data.Aeson (Value,object,(.=),toJSON)
+import Data.Aeson.Types (Pair)
+import Data.Text (Text)
 
 import Data.Map (Map,traverseWithKey,mapEither)
 import qualified Data.Map as Map (fromList)
+import qualified Data.Set as Set (toList)
 
 import Control.Monad (void,forM,forM_)
 
@@ -103,8 +109,20 @@ insertDeclarations packagename versionnumber modulename declarations = do
         \CREATE UNIQUE (version)-[:MODULE]->(module:Module {modulenname : {modulename}})\
         \FOREACH (declarationdata IN {declarations} |\
         \    CREATE UNIQUE (module)-[:DECLARATION]->(declaration:Declaration {\
-        \        genre : {declarationdata}.genre,\
-        \        declarationast : {declarationdata}.declarationast}))"
+        \        declarationgenre : declarationdata.declarationgenre,\
+        \        declarationast : declarationdata.declarationast})\
+        \    FOREACH (declaredsymboldata IN declarationdata.declaredsymbols |\
+        \        MERGE (declaredsymbol:Symbol {\
+        \            symbolgenre : declaredsymboldata.symbolgenre,\
+        \            symbolmodule : declaredsymboldata.symbolmodule,\
+        \            symbolname : declaredsymboldata.symbolname})\
+        \        CREATE UNIQUE (declaration)-[:DECLAREDSYMBOL]->(declaredsymbol))\
+        \    FOREACH (usedsymboldata IN declarationdata.usedsymbols |\
+        \        MERGE (usedsymbol:Symbol {\
+        \            symbolgenre : usedsymboldata.symbolgenre,\
+        \            symbolmodule : usedsymboldata.symbolmodule,\
+        \            symbolname : usedsymboldata.symbolname})\
+        \        CREATE UNIQUE (declaration)-[:USEDSYMBOL]->(usedsymbol)))"
         (object [
             "packagename" .= packagename,
             "versionnumber" .= display versionnumber,
@@ -113,9 +131,48 @@ insertDeclarations packagename versionnumber modulename declarations = do
     return ()
 
 declarationData :: Declaration -> Value
-declarationData (Declaration genre declarationast _ _) = object [
-    "genre" .= show genre,
-    "declarationast" .= declarationast]
+declarationData (Declaration genre declarationast declaredsymbols usedsymbols) = object [
+    "declarationgenre" .= show genre,
+    "declarationast" .= declarationast,
+    "declaredsymbols" .= symbolsData declaredsymbols,
+    "usedsymbols" .= symbolsData usedsymbols]
+
+symbolsData :: Symbols -> Value
+symbolsData (Symbols symvalueinfos symtypeinfos) = toJSON (
+    map symValueData (Set.toList symvalueinfos) ++
+    map symTypeData (Set.toList symtypeinfos))
+
+symValueData :: SymValueInfo OrigName -> Value
+symValueData symvalueinfo = object [
+    "symbolgenre" .= symValueGenre symvalueinfo,
+    "symbolmodule" .= symModuleData (sv_origName symvalueinfo),
+    "symbolname" .= symNameData (sv_origName symvalueinfo)]
+
+symTypeData :: SymTypeInfo OrigName -> Value
+symTypeData symtypeinfo = object [
+    "symbolgenre" .= symTypeGenre symtypeinfo,
+    "symbolmodule" .= symModuleData (st_origName symtypeinfo),
+    "symbolname" .= symNameData (st_origName symtypeinfo)]
+
+symModuleData :: OrigName -> String
+symModuleData (OrigName _ (GName originalmodulename _)) = originalmodulename
+
+symNameData :: OrigName -> String
+symNameData (OrigName _ (GName _ symbolname)) = symbolname
+
+symValueGenre :: SymValueInfo a -> Text
+symValueGenre (SymValue _ _) = "Value"
+symValueGenre (SymMethod _ _ _) = "Method"
+symValueGenre (SymSelector _ _ _ _) = "Selector"
+symValueGenre (SymConstructor _ _ _) = "Constructor"
+
+symTypeGenre :: SymTypeInfo a -> Text
+symTypeGenre (SymType _ _) = "Type"
+symTypeGenre (SymData _ _) = "Data"
+symTypeGenre (SymNewType _ _) = "Newtype"
+symTypeGenre (SymTypeFam _ _) = "TypeFamily"
+symTypeGenre (SymDataFam _ _) = "DataFamily"
+symTypeGenre (SymClass _ _) = "Class"
 
 insertModuleError :: (Monad m) => PackageName -> VersionNumber -> ModuleName -> ModuleError -> NeoT m ()
 insertModuleError packagename versionnumber modulename moduleerror = do
