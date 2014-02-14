065ce1669b508f9a1e38564ea7494fcde28cf93e
phischu <pschuster@uni-koblenz.de>
2014-02-14 14:02:55 +0100

    one big query

--- src/Insertion.hs
+++ src/Insertion.hs
@@ -26,15 +26,16 @@ import Data.Text (Text)
 import qualified  Data.ByteString as ByteString (concat)
 
 import Data.Map (Map,mapWithKey,mapEither)
-import qualified Data.Map as Map (fromList)
+import qualified Data.Map as Map (fromList,toList)
 import qualified Data.Set as Set (toList)
 
 import Data.Foldable (fold)
 import Control.Monad (void,forM,forM_,when)
 
 insertAllPackages :: ParsedRepository -> IO ()
-insertAllPackages =
-    void . (traverseRepository (\packagename versionnumber packagepath -> do
+insertAllPackages parsedrepository = do
+    createIndex
+    flip traverseRepository parsedrepository (\packagename versionnumber packagepath -> do
         maybepackageinformation <- loadPackage packagepath
         case maybepackageinformation of
             Nothing -> return ()
@@ -42,7 +43,8 @@ insertAllPackages =
             Just (PackageInformation modulenames dependencies) -> do
                 modulemap <- loadModuleDeclarations packagepath modulenames >>= return . Map.fromList
                 maybenameerrors <- loadNameErrors packagepath
-                insertPackage packagename versionnumber dependencies modulemap maybenameerrors))
+                insertPackage packagename versionnumber dependencies modulemap maybenameerrors)
+    return ()
 
 loadModuleDeclarations :: PackagePath -> [ModuleName] -> IO [(ModuleName,Either ModuleError [Declaration])]
 loadModuleDeclarations packagepath modulenames = forM modulenames (\modulename -> do
@@ -55,6 +57,10 @@ loadModuleDeclarations packagepath modulenames = forM modulenames (\modulename -
             case maybedeclarations of
                 Nothing -> return (modulename,Left DeclarationsFileError)
                 Just declarations -> return (modulename,Right declarations))
+
+createIndex :: IO ()
+createIndex = batchInsert [
+    cypher "CREATE INDEX ON :Symbol(symbolname)" (object [])]
 
 batchInsert :: [Value] -> IO ()
 batchInsert apicalls = do
@@ -91,69 +97,72 @@ insertPackage ::
 insertPackage packagename versionnumber dependencies modulemap maybenameerrors = do
     putStrLn ("Inserting: " ++ packagename ++ " " ++ display versionnumber)
     let (moduleerrormap,declarationsmap) = splitModuleMap modulemap
-    batchInsert (concat [
-        [insertDependencies packagename versionnumber dependencies],
-        fold (mapWithKey (\modulename declarations ->
-            [insertDeclarations packagename versionnumber modulename declarations]) declarationsmap),
-        fold (mapWithKey (\modulename moduleerror ->
-            [insertModuleError packagename versionnumber modulename moduleerror]) moduleerrormap),
-        [insertNameErrors packagename versionnumber maybenameerrors]])
+    batchInsert ([cypher
+        "MERGE (rootnode:ROOTNODE)\
+        \MERGE (rootnode)-[:PACKAGE]->(package:Package {packagename : {packagename}})\
+        \CREATE (package)-[:VERSION]->(version:Version {versionnumber : {versionnumber}})\
+        \ \
+        \FOREACH (dependencyname IN {dependencynames} |\
+        \    MERGE (rootnode)-[:PACKAGE]->(otherpackage:Package {packagename : dependencyname})\
+        \    CREATE (version)-[:DEPENDENCY]->(otherpackage))\
+        \ \
+        \FOREACH (moduleerrordata IN {moduleerrormap} |\  
+        \    CREATE (version)-[:MODULE]->(module:Module {modulenname : moduleerrordata.modulename})\
+        \    CREATE (module)-[:MODULEERROR]->(moduleerror:ModuleError {moduleerror : moduleerrordata.moduleerrorstring}))\
+        \ \
+        \FOREACH (nameerrorstring IN {nameerrors} |\
+        \    CREATE (version)-[:NAMEERROR]->(nameerror:NameError {nameerror : nameerrorstring}))\
+        \ \
+        \FOREACH (moduledata IN {modulemap} |\
+        \    CREATE (version)-[:MODULE]->(module:Module {modulenname : moduledata.modulename})\
+        \ \
+        \    FOREACH (declarationdata IN moduledata.declarations |\
+        \        CREATE (module)-[:DECLARATION]->(declaration:Declaration {\
+        \            declarationgenre : declarationdata.declarationgenre,\
+        \            declarationast : declarationdata.declarationast})\
+        \        FOREACH (declaredsymboldata IN declarationdata.declaredsymbols |\
+        \            MERGE (declaredsymbol:Symbol {\
+        \                symbolgenre : declaredsymboldata.symbolgenre,\
+        \                symbolmodule : declaredsymboldata.symbolmodule,\
+        \                symbolname : declaredsymboldata.symbolname})\
+        \            CREATE (declaration)-[:DECLAREDSYMBOL]->(declaredsymbol))\
+        \        FOREACH (usedsymboldata IN declarationdata.usedsymbols |\
+        \            MERGE (usedsymbol:Symbol {\
+        \                symbolgenre : usedsymboldata.symbolgenre,\
+        \                symbolmodule : usedsymboldata.symbolmodule,\
+        \                symbolname : usedsymboldata.symbolname})\
+        \            CREATE (declaration)-[:USEDSYMBOL]->(usedsymbol))))"
+        (object [
+            "packagename" .= packagename,
+            "versionnumber" .= display versionnumber,
+            "dependencynames" .= map (\(Dependency dependencyname _) -> dependencyname) dependencies,
+            "moduleerrormap" .= [object [
+                "modulename" .= display modulename,
+                "moduleerrorstring" .= show moduleerrorstring] | (modulename,moduleerrorstring) <- Map.toList moduleerrormap],
+            "nameerrors" .= case maybenameerrors of
+                Just (NameErrors nameerrors) -> nameerrors
+                Nothing -> [],
+            "modulemap" .= [object [
+                "modulename" .= display modulename,
+                "declarations" .= map declarationData declarations] | (modulename,declarations) <- Map.toList declarationsmap]])])
+
+
 
 insertPackageError :: PackageName -> VersionNumber -> PackageError -> IO ()
 insertPackageError packagename versionnumber packageerror = batchInsert ([
         cypher
             "MERGE (rootnode:ROOTNODE)\
-            \CREATE UNIQUE (rootnode)-[:PACKAGE]->(package:Package {packagename : {packagename}})\
-            \CREATE UNIQUE (package)-[:VERSION]->(version:Version {versionnumber : {versionnumber}})\
-            \CREATE UNIQUE (version)-[:PACKAGEERROR]->(packageerror:PackageError {packageerror : {packageerrorstring}})"
+            \CREATE UNIQUE (rootnode)-[:PACKAGE]->\
+            \              (package:Package {packagename : {packagename}})-[:VERSION]->\
+            \              (version:Version {versionnumber : {versionnumber}})-[:PACKAGEERROR]->\
+            \              (packageerror:PackageError {packageerror : {packageerrorstring}})"
             (object [
                 "packagename" .= packagename,
                 "versionnumber" .= display versionnumber,
                 "packageerrorstring" .= show packageerror])])
 
-insertDependencies :: PackageName -> VersionNumber -> [Dependency] -> Value
-insertDependencies packagename versionnumber dependencies = cypher
-        "MERGE (rootnode:ROOTNODE)\
-        \CREATE UNIQUE (rootnode)-[:PACKAGE]->(package:Package {packagename : {packagename}})\
-        \CREATE UNIQUE (package)-[:VERSION]->(version:Version {versionnumber : {versionnumber}})\
-        \FOREACH (dependencyname IN {dependencynames} |\
-        \    CREATE UNIQUE (rootnode)-[:PACKAGE]->(otherpackage:Package {packagename : dependencyname})\
-        \    CREATE UNIQUE (version)-[:DEPENDENCY]->(otherpackage))"
-        (object [
-            "packagename" .= packagename,
-            "versionnumber" .= display versionnumber,
-            "dependencynames" .= map (\(Dependency dependencyname _) -> dependencyname) dependencies])
-
 splitModuleMap :: Map ModuleName (Either ModuleError [Declaration]) -> (Map ModuleName ModuleError,Map ModuleName [Declaration])
 splitModuleMap = mapEither id
-
-insertDeclarations :: PackageName -> VersionNumber -> ModuleName -> [Declaration] -> Value
-insertDeclarations packagename versionnumber modulename declarations = cypher
-        "MERGE (rootnode:ROOTNODE)\
-        \CREATE UNIQUE (rootnode)-[:PACKAGE]->(package:Package {packagename : {packagename}})\
-        \CREATE UNIQUE (package)-[:VERSION]->(version:Version {versionnumber : {versionnumber}})\
-        \CREATE UNIQUE (version)-[:MODULE]->(module:Module {modulenname : {modulename}})\
-        \FOREACH (declarationdata IN {declarations} |\
-        \    CREATE UNIQUE (module)-[:DECLARATION]->(declaration:Declaration {\
-        \        declarationgenre : declarationdata.declarationgenre,\
-        \        declarationast : declarationdata.declarationast})\
-        \    FOREACH (declaredsymboldata IN declarationdata.declaredsymbols |\
-        \        MERGE (declaredsymbol:Symbol {\
-        \            symbolgenre : declaredsymboldata.symbolgenre,\
-        \            symbolmodule : declaredsymboldata.symbolmodule,\
-        \            symbolname : declaredsymboldata.symbolname})\
-        \        CREATE UNIQUE (declaration)-[:DECLAREDSYMBOL]->(declaredsymbol))\
-        \    FOREACH (usedsymboldata IN declarationdata.usedsymbols |\
-        \        MERGE (usedsymbol:Symbol {\
-        \            symbolgenre : usedsymboldata.symbolgenre,\
-        \            symbolmodule : usedsymboldata.symbolmodule,\
-        \            symbolname : usedsymboldata.symbolname})\
-        \        CREATE UNIQUE (declaration)-[:USEDSYMBOL]->(usedsymbol)))"
-        (object [
-            "packagename" .= packagename,
-            "versionnumber" .= display versionnumber,
-            "modulename" .= display modulename,
-            "declarations" .= map declarationData declarations])
 
 declarationData :: Declaration -> Value
 declarationData (Declaration genre declarationast declaredsymbols usedsymbols) = object [
@@ -198,33 +207,4 @@ symTypeGenre (SymNewType _ _) = "Newtype"
 symTypeGenre (SymTypeFam _ _) = "TypeFamily"
 symTypeGenre (SymDataFam _ _) = "DataFamily"
 symTypeGenre (SymClass _ _) = "Class"
-
-insertModuleError :: PackageName -> VersionNumber -> ModuleName -> ModuleError -> Value
-insertModuleError packagename versionnumber modulename moduleerror = cypher
-        "MERGE (rootnode:ROOTNODE)\
-        \CREATE UNIQUE (rootnode)-[:PACKAGE]->(package:Package {packagename : {packagename}})\
-        \CREATE UNIQUE (package)-[:VERSION]->(version:Version {versionnumber : {versionnumber}})\
-        \CREATE UNIQUE (version)-[:MODULE]->(module:Module {modulenname : {modulename}})\
-        \CREATE UNIQUE (module)-[:MODULEERROR]->(moduleerror:ModuleError {moduleerror : {moduleerrorstring}})"
-        (object [
-            "packagename" .= packagename,
-            "versionnumber" .= display versionnumber,
-            "modulename" .= display modulename,
-            "moduleerrorstring" .= show moduleerror])
-
-insertNameErrors :: PackageName -> VersionNumber -> Maybe NameErrors -> Value
-insertNameErrors packagename versionnumber maybenameerrors = cypher
-        "MERGE (rootnode:ROOTNODE)\
-        \CREATE UNIQUE (rootnode)-[:PACKAGE]->(package:Package {packagename : {packagename}})\
-        \CREATE UNIQUE (package)-[:VERSION]->(version:Version {versionnumber : {versionnumber}})\
-        \FOREACH (nameerrorstring IN {nameerrors} |\
-        \    CREATE UNIQUE (version)-[:NAMEERROR]->(nameerror:NameError {nameerror : nameerrorstring}))"
-        (object [
-            "packagename" .= packagename,
-            "versionnumber" .= display versionnumber,
-            "nameerrors" .= nameerrors])
-        where
-            nameerrors = case maybenameerrors of
-                Just (NameErrors nameerrors) -> nameerrors
-                Nothing -> []
 
